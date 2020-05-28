efa742c86351ae6420a49af39330a63830998b56
Chris Worfolk <chris@worfolk.co.uk>
2020-05-28 15:15:39 +0100

    DQL version of homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,6 +9,19 @@ class DefaultController extends Controller
 {
     public function homepage(Request $request, Response $response)
     {
+        $dql = "SELECT a FROM App\Entity\Article a
+                WHERE a.published <= CURRENT_TIMESTAMP()
+                ORDER BY a.published DESC";
+        $query = $this->ci->get('db')->createQuery($dql);
+        $articles = $query->getResult();
+
+        return $this->renderPage($response, 'homepage.html', [
+            'articles' => $articles
+        ]);
+    }
+
+    public function homepageOld(Request $request, Response $response)
+    {
         $articles = $this->ci->get('db')->getRepository('App\Entity\Article')->findBy([], [
             'published' => 'DESC'
         ]);
