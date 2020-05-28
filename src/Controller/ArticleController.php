5ad97f2da511783d89e797f436d26935897fa7bc
Chris Worfolk <chris@worfolk.co.uk>
2020-05-28 14:51:20 +0100

    View by slug

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,12 +4,26 @@ namespace App\Controller;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-
 use Slim\Exception\HttpNotFoundException;
 
 class ArticleController extends Controller
 {
-    public function view(Request $request, Response $response)
+    public function view(Request $request, Response $response, $args)
+    {
+        $article = $this->ci->get('db')->getRepository('App\Entity\Article')->findOneBy([
+            'slug' => $args['slug']
+        ]);
+
+        if (!$article) {
+            throw new HttpNotFoundException($request);
+        }
+
+        return $this->renderPage($response, 'article.html', [
+            'article' => $article
+        ]);
+    }
+
+    public function viewPk(Request $request, Response $response)
     {
         $article = $this->ci->get('db')->find('App\Entity\Article', 1);
 
